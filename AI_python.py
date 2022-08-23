6f79317321bf7102905b35efb71817d2c6e0a10b
leedakoog <leedakoog@naver.com>
2022-08-23 16:27:05 +0900

    인공신경망 함수 스테레오타입

--- AI_python.py
+++ AI_python.py
@@ -9,51 +9,31 @@ NUM_SAMPLES = 1000
 
 np.random.seed(int(time.time()))
 
-xs = np.random.uniform(-2, 2, NUM_SAMPLES)
+xs = np.random.uniform(0.1, 5, NUM_SAMPLES)
 np.random.shuffle(xs)
-print(xs[:5])
 
-ys = (xs + 1.7)*(xs + 0.7)*(xs - 0.3)*(xs - 1.3)*(xs - 1.9) + 0.2
-print(ys[:5])
-
-#plt.plot(xs, ys, 'b. ')
-#plt.show()
-
-ys += 0.1*np.random.randn(NUM_SAMPLES)
-
-#plt.plot(xs, ys, 'g. ')
-#plt.show()
+ys = 1.0/xs
+ys += 0.1*np.random.randn(NUM_SAMPLES) #8~16 인공 신경망 학습에 사용할 데이터 생성
 
 NUM_SPLIT = int(0.8*NUM_SAMPLES)
 
-x_train, x_test = np.split(xs, [NUM_SPLIT]) #앞의 것에 800개
-y_train, y_test = np.split(ys, [NUM_SPLIT])
+x_train, x_test = np.split(xs, [NUM_SPLIT])
+y_train, y_test = np.split(ys, [NUM_SPLIT]) #18~21 데이터를 훈련 데이터와 실험 데이터로 나눔
 
-#plt.plot(x_train, y_train, 'b. ', label = 'train')
-#plt.plot(x_test, y_test, 'r. ', label = 'test')
-#plt.legend()
-#plt.show()
-model_f = tf.keras.Sequential([  #keras 라이브러리에서 제공하는 tensor를 생성하고, 입력 노드의 개수를 정해줌.
+model_f = tf.keras.Sequential([
     tf.keras.layers.InputLayer(input_shape = (1,)),
-    tf.keras.layers.Dense(16, activation = 'relu'), #16개의 노드생성
-    tf.keras.layers.Dense(16, activation = 'relu'), #Dense는 내부적으로 y = activation(x*w + b)식을 생성
-    tf.keras.layers.Dense(1) #출력 신경망 생성
-])
+    tf.keras.layers.Dense(16, activation = 'relu'),
+    tf.keras.layers.Dense(16, activation = 'relu'),
+    tf.keras.layers.Dense(1)
+]) # 23~28 인공 신경망 구성에 필요한 입력 층, 은닉 층, 출력 층을 구성함
 
-model_f.compile(optimizer = 'rmsprop', loss = 'mse')
+model_f.compile(optimizer = 'sgd', loss = 'mean_squared_error') #인공신경망 내부 망을 구성하고, 학습에 필요한 오차함수, 최적화함수를 설정
 
-p_test = model_f.predict(x_test)
+model_f.fit(x_train, y_train, epochs = 100) #인공신경망 학습
 
-# plt.plot(x_test, y_test, 'b. ', label = 'actual')
-# plt.plot(x_test, p_test, 'r. ', label = 'predicted')
-# plt.legend()
-# plt.show()
-
-model_f.fit(x_train, y_train, epochs = 200) #x_train, y_train 데이터에 맞도록 학습함을 의미, epochs는 학습 횟수.
-
-p_test = model_f.predict(x_test)
+p_test = model_f.predict(x_test) # 학습시킨 인공 신경망을 이용하여 새로 들어온 데이터에 대한 예측을 수행함
 
 plt.plot(x_test, y_test, 'b. ', label = 'actual')
 plt.plot(x_test, p_test, 'r. ', label = 'predicted')
 plt.legend()
-plt.show()
+plt.show()  #나머지는 출력
